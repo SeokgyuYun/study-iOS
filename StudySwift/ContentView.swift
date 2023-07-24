9ed7c7d45456a82772a733a1782923f56ffabab4
윤석규 <greeentea.dev@gmail.com>
2023-07-25 00:50:37 +0900

    init :: project

---
+++ StudySwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudySwift
+//
+//  Created by 윤석규 on 7/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudySwift/StudySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftApp.swift
+//  StudySwift
+//
+//  Created by 윤석규 on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
